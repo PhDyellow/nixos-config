b71ca4ad4adfed5feacb56316c7e4523a6222b32
Phil Dyer <phildyer@protonmail.com>
2023-02-01 15:26:28 +1000

    Initial flake commit

---
+++ flake.nix
@@ -0,0 +1,200 @@
+{
+  description = "Prime-AI Nixos Configuration";
+
+  inputs  = {
+    nixpkgs-unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    systemd-secure-boot = {
+      flake = false;
+      type = "github";
+      owner = "PhDyellow";
+      repo = "nix-machines";
+      ref = "main";
+    };
+
+  };
+
+  outputs = {self, ...}@inputs:
+    let
+
+      hardware_config = { config, lib, pkgs, modulesPath, ...}:
+        {
+          imports = [
+              (modulesPath + "/installer/scan/not-detected.nix")
+          ];
+
+          boot.initrd.luks.devices."nixos-crypt".device =
+            "dev/disk/by-uuid/c4129dcf-90da-4d0c-8da9-880b9c111e6f";
+
+          boot.initrd.availableKernelModules = [
+            "nvme"
+            "xhci_pci"
+            "ahci"
+            "sdhci_pci"
+          ];
+          boot.initrd.kernelModules = [ ];
+          boot.kernelModules = [
+            "kvm-amd"
+          ];
+          boot.extraModulePackages = [ ];
+
+
+          filesystems = {
+            "/" = {
+              device = "/dev/disk/by-uuid/6a9829dd-f9b5-4f87-8e1b-5df21fadfeda";
+              fsType = "btrfs";
+            };
+            "/boot" = {
+              device = "systemd-1";
+              fsType = "autofs";
+            };
+          };
+
+          swapDevices = [ ];
+
+          hardware = {
+            cpu.amp.updateMicrocode = lib.mkDefault config.hardware.enableRedistibutableFirmware;
+            video.hidpi.enable = lib.mkDefault true;
+          };
+        };
+
+      system_config = {config, pkgs, ...}:
+        {
+          nixpkgs.config.allowUnfree = true;
+          system.stateVersion = "21.11";
+          boot.loader.systemd-boot = {
+            enable = true;
+            configurationLimit = 200; #limit to 200 versions to boot from: 200*30Mb = 6Gb out of 10GB partition
+            editor = false;  #don't allow kernel cli editing before boot
+            #secureBoot = {
+            #  enable = false;
+            #  keyPath = "/secrets/secureboot_keys/DB.key";
+            #  certPath = "/secrets/secureboot_keys/DB.crt";
+            #};
+          };
+          boot.loader.efi.canTouchEfiVariables = true;
+          boot.kernelPackages = pkgs.linuxPackages_latest;
+
+          age.secrets.wpa_pwd_env.file = "/secrets/agenix/wpa_pwd.env.age";
+          networking = {
+            hostName = "prime-ai-nixos";
+            firewall = {
+              allowedTCPPorts = [ ];
+              allowedUDPPorts = [ ];
+            };
+            wireless = {
+              enable = true;
+              environmentFile = config.age.secrets.wpa_pwd_env.path;
+              networks = {
+                PBAGJmob = {
+                  psk = "@phone_psk@";
+                  priority = 10;
+                };
+                WIFI-56E0-5G = {
+                  psk = "@parent_psk@";
+                  priority = 60;
+                };
+                PBAGJE_H_5G = {
+                  psk = "@home_psk@";
+                  priority = 99;
+                };
+              };
+            };
+            interfaces = {
+              enp4s0.useDHCP = true;
+              wlp5s0.useDHCP = true;
+            };
+          };
+
+          time.timeZone = "Australia/Brisbane";
+
+          services.xserver = {
+            enable = true;
+            displayManager.lightdm.enable = true;
+            displayManager.defaultSession = "none+dwm";
+            windowManager.dwm.enable = true;
+            videoDrivers = ["nvidia"]; #noveau gave blank screen, nvidia needed nixpkgs.config.allowUnfree
+          };
+
+          sound.enable = true;
+          hardware.pulseaudio.enable = true;
+
+          users.mutableUsers = false;
+          age.secrets.user_phil_pwd.file = "/secrets/agenix/user_phil_pwd.age";
+          users.users = {
+            phil = {
+              isNormalUser = true;
+              extraGroups = ["wheel"];
+              #passwordFile = config.age.secrets.user_phil_pwd.path;
+              password = "tmppwd";
+            };
+            root.hashedPassword = "*";
+          };
+
+          environment.systemPackages = with pkgs; [
+            vim
+            git
+            gitSVN
+            wget
+            firefox
+            st
+            inputs.agenix.defaultPackage.x86_64-linux
+            sbsigntool
+            efitools
+            openssl
+            python3
+          ];
+
+          nix = {
+            package = pkgs.nixVersions.unstable;
+            settings = {
+              system-features = [
+                "recursive-nix"
+                "kvm"
+              ];
+              experimental-features = [
+                "nix=command"
+                "flakes"
+                "recursive-nix"
+              ];
+            };
+          };
+
+          #Enable OpenSSH daemon
+          #Primary use here is for agenix.
+          #Enabling openssh creates host keys, which agenix uses for secrets
+          #password entry is therefore disabled, and firewall ports are not opened
+          services.openssh = {
+            enable = true;
+            passwordAuthentication = false;
+            openFirewall = false;
+          };
+
+
+
+    in  {
+      nixosConfigurations = {
+        prime-ai = nixpkgs-unstable.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (hardware_config)
+            (system_config)
+          ];
+
+    };
+
+
+
+
+
+
+
+
+}
