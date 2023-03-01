0d0ad5817a40f58779affb7c2b401f9a7f7087f8
Phil Dyer <phildyer@protonmail.com>
2023-03-01 16:24:04 +1000

    fix tuigreet settings

--- flake.nix
+++ flake.nix
@@ -366,7 +366,7 @@
             enable = true;
             settings = {
                 default_session = {
-                  command = "${pkgs.greetd.tuigreet}/bin/tuigreet -t- r -g 'Init: Prime-AI' --cmd Hyprland";
+                  command = "${pkgs.greetd.tuigreet}/bin/tuigreet -t -r -g 'Init: Prime-AI' --cmd Hyprland";
                   user = "phil";
                 };
             };
