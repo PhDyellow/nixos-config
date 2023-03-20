96555e73edffef8469ff948f547c3261e6203e24
Phil Dyer <phildyer@protonmail.com>
2023-03-20 10:39:13 +1000

    Using gnome requires things shut down

--- flake.nix
+++ flake.nix
@@ -472,14 +472,14 @@ systemd = {
 
           #pipewire specific config
           #security.rtkit.enable = true;
-          services.pipewire = {
-            #enable = true;
-            alsa.enable = true;
-            alsa.support32Bit = true;
-            pulse.enable = true;
-            # If you want to use JACK applications, uncomment this
-            #jack.enable = true;
-          };
+          # services.pipewire = {
+          #   #enable = true;
+          #   alsa.enable = true;
+          #   alsa.support32Bit = true;
+          #   pulse.enable = true;
+          #   # If you want to use JACK applications, uncomment this
+          #   #jack.enable = true;
+          # };
 
 
           #Enable polkit for passwords, and activate agent
