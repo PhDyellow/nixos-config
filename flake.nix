81d6af75ef0dd89895ce3cc33700810e8c4e84cb
Phil Dyer <phildyer@protonmail.com>
2023-03-23 11:46:10 +1000

    find upscaleDefaultCursor bug 2

--- flake.nix
+++ flake.nix
@@ -137,14 +137,14 @@
             };
           };
           #services.xserver.windowManager.dwm.enable = true;
-          services.xserver = {
-            enable = true;
-            desktopManager = {
-              xterm.enable = false;
-              xfce.enable = true;
-            };
-            displayManager.defaultSession = "xfce";
-          };
+          # services.xserver = {
+          #   enable = true;
+          #   desktopManager = {
+          #     xterm.enable = false;
+          #     xfce.enable = true;
+          #   };
+          #   displayManager.defaultSession = "xfce";
+          # };
           boot = {
             #The next line may fix a system crash in nvidia 525.xx.xx
             #Nvidia has enabled a new feature in 510, GSP, but logs
