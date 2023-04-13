5adca90c3411cb915e6c3f2b1131e028b6195b01
Phil Dyer <phildyer@protonmail.com>
2023-04-13 16:10:43 +1000

    set lsix font size

--- flake.nix
+++ flake.nix
@@ -59,7 +59,8 @@
           postInstall = ''
             substituteInPlace $out/bin/lsix \
               --replace tilesize=120 tilesize=500 \
-              --replace \#fontfamily=Dejavu-Sans fontfamily=Dejavu-Sans
+              --replace \#fontfamily=Dejavu-Sans fontfamily=Dejavu-Sans \
+              --replace fontsize=\$((tilewidth/10)) fontsize=20
           '';
         });
       };
