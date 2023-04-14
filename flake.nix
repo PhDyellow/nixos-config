eb8d0072a39135ecbed68d5d39c1f0e568e05c85
Phil Dyer <phildyer@protonmail.com>
2023-04-14 11:57:29 +1000

    fix emacs init excess paren

--- flake.nix
+++ flake.nix
@@ -924,7 +924,7 @@
                       config = ''
                           (add-to-list 'backup-directory-alist (cons tramp-file-name-regexp nil))
                           ;; Add the remote's PATH to tramp's search path (why isn't this the default?)
-                          (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+                          (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
                       '';
                     };
                     mouse = {
