247929cc02941e55f4905be404d292aee71b7edf
Phil Dyer <phildyer@protonmail.com>
2023-04-24 13:37:40 +1000

    Fix excess brackets

--- flake.nix
+++ flake.nix
@@ -947,15 +947,15 @@
 		      enable = true;
 		      bind = {
 		        "C-h B" = "embark-bindings";
-			};
-			config = ''
+                      };
+		      config = ''
 			    ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
-                 (window-parameters (mode-line-format . none)))))
+                 (window-parameters (mode-line-format . none))))
 
-			''};
+		      '';
 		    };
 		    embark-consult = {
 		      enable = true;
