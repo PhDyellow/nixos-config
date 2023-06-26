ac2607ba1bc8dce1711c7ecb551b0a3e261c5d31
Phil Dyer <phildyer@protonmail.com>
2023-06-26 19:03:38 +1000

    Fix syncthing configDir path

--- flake.nix
+++ flake.nix
@@ -494,7 +494,7 @@
           services.syncthing = {
             enable = true;
             dataDir = "/para/resources/syncthing";
-            configDir = "home/phil/.config/syncthing";
+            configDir = "/home/phil/.config/syncthing";
             overrideDevices = true;
             overrideFolders = true;
             devices = {
