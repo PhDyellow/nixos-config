b156ea9e6d2aec528ffdfe17c519c431adcd2cec
Phil Dyer <phildyer@protonmail.com>
2023-10-27 14:37:15 +1000

    Create minimal working emacs module, but disable normally

--- flake.nix
+++ flake.nix
@@ -970,6 +970,7 @@
             in [
               nurNoPkgs.repos.rycee.hmModules.emacs-init
               self.hmModules.emacs-hm-init
+              # self.hmModules.emacs-mwe
               self.hmModules.gtk_setup
               self.hmModules.gpg-agent-emacs
               self.hmModules.hyprland-config
@@ -3504,6 +3505,66 @@ Close when conclusion is reached.
                         };
         };
       };
+      emacs-mwe = {config, pkgs, ...}:
+        {
+          services = {
+            emacs = {
+              enable = true;
+              defaultEditor = true;
+              ## Allow server to start with graphics so wayland session is
+              ## correctly detected by server
+              # socketActivation.enable = true;
+              startWithUserSession = "graphical";
+            };
+          };
+          programs = {
+            emacs = {
+              enable = true;
+              package = pkgs.emacs-pgtk;
+              extraPackages = epkgs: [
+              ];
+              init = {
+                enable = true;
+                packageQuickstart = true;
+                recommendedGcSettings = true;
+                startupTimer = true;
+                earlyInit = "";
+                postlude = ''
+                      ; Seems to break if called too early
+                      ; (citar-org-roam-mode)
+
+                      ;; Local Variables:
+                      ;; End:
+                    '';
+                usePackage = {
+                    djvu = {
+                      enable = true;
+                    };
+
+                  org-noter = {
+                      after = [ "org" "pdf-tools" "nov" "djvu"];
+                      enable = true;
+                      command = [ "org-noter" ];
+                    };
+                    pdf-tools = {
+                      enable = true;
+                      init = ''
+                      '';
+                    };
+                    pdf-loader = { #part of pdf-tools
+                      enable = true;
+                      config = ''
+                        (pdf-loader-install)
+                      '';
+                    };
+
+                };
+
+              };
+            };
+
+          };
+        };
       tex-full = {config, pkgs, ...}:
         {
           programs.texlive = {
