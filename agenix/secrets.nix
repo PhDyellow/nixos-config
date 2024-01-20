9d308af74cd0d373b76d8d7186ea7b35e248b480
Phil Dyer <phildyer@protonmail.com>
2024-01-20 13:56:48 +1000

    Add munge key and update agenix keys

--- agenix/secrets.nix
+++ agenix/secrets.nix
@@ -1,6 +1,6 @@
 let 
-  prime-ai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXuv00yB0lA43P4HGyTkund20jlbZvmV88uT3XcRRJM root@prime-ai-nixos";
-  prime-ai-phil = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDi6zlsK1OVU71zsgMRBzF48XKIj6zo24WCOYA+VrKir phil_prime_ai@nixos";
+  prime-ai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXuv00yB0lA43P4HGyTkund20jlbZvmV88uT3XcRRJM";
+  prime-ai-phil = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDi6zlsK1OVU71zsgMRBzF48XKIj6zo24WCOYA+VrKir";
   all_systems = [prime-ai prime-ai-phil];
 in
 {
@@ -9,4 +9,5 @@ in
   "cifs_dpbagje_share.age".publicKeys = all_systems;
   "prime_ai_tailscale.age".publicKeys = [prime-ai prime-ai-phil];
   "github_pat.age".publicKeys = all_systems;
+  "munge_key.age".publicKeys = all_systems;
 }
