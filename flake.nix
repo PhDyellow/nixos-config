171376299ca55d48d70b9bbb83daa8e2efcfb720
Phil Dyer <phildyer@protonmail.com>
2024-05-01 11:57:11 +1000

    Fix boot label for VM

--- flake.nix
+++ flake.nix
@@ -417,7 +417,7 @@
             };
             "/boot" = {
               fsType = "fat";
-              device = "/dev/disk/by-label/BOOT";
+              device = "/dev/disk/by-label/BOOTPART";
             };
           };
           swapDevices = [
