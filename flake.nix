042262bb4526048fc921706b9d291e4bc958a973
Phil Dyer <phildyer@protonmail.com>
2024-08-15 13:28:30 +1000

    Remove deprecated option `hardware.opengl.driSupport`

--- flake.nix
+++ flake.nix
@@ -857,8 +857,6 @@
               };
               opengl = {
                 enable = true;
-                driSupport = true;
-                driSupport32Bit = true;
                 extraPackages = with pkgs; [
                   vaapiVdpau
                   libvdpau-va-gl
