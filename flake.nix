676b521f0c29ae16748874a89d65e314c22e32b9
Phil Dyer <phildyer@protonmail.com>
2024-08-20 12:30:23 +1000

    Agenix set to access /persistent/etc/ssh directly
    The impermanence module occurs much later in the boot process, so
    /etc/ssh is not available and agenix fails.

--- flake.nix
+++ flake.nix
@@ -889,7 +889,19 @@
               mprime
             ];
           };
-
+        impermanence-agenix = {config, lib, pkgs, ...}:
+          {
+            imports = [
+              inputs.ragenix.nixosModules.age
+            ];
+            # Under impermanence, /persistent/etc/ssh is not
+            # mounted to /etc/ssh when age attempts to decrypt
+            # secrets
+            age.identityPaths = [
+              "/persistent/etc/ssh/ssh_host_ed25519_key"
+              "/persistent/etc/ssh/ssh_host_rsa_key"
+            ];
+          };
         impermanence = {config, lib, pkgs, ...}:
           {
             imports = [
