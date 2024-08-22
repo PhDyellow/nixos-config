77ec605620c3c848de32f1625ceedcb5f182356b
Phil Dyer <phildyer@protonmail.com>
2024-08-22 13:34:39 +1000

    Force not making multi-user.target wait for syncthing

--- flake.nix
+++ flake.nix
@@ -910,10 +910,10 @@
                 "network.target" # standard for syncthing
                 "multi-user.target" # paths.target waits for persistence
               ];
-              wantedBy = []; # Not an error if syncthing fails
+              lib.mkForce wantedBy = []; # Not an error if syncthing fails
             };
             systemd.services.syncthing = {
-              wantedBy = []; # Not an error if syncthing fails
+              lib.mkForce wantedBy = []; # Not an error if syncthing fails
             };
           };
         impermanence = {config, lib, pkgs, ...}:
