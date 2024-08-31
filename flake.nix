d22f4ab596fa5c81a44db0080e9715b99afee5f9
Phil Dyer <phildyer@protonmail.com>
2024-09-01 06:17:23 +1000

    Fix mismatch between hyprland config structure and nix structure
    In nix, `.` is always accessing a value in a nested attribute set.
    
    Hyprland uses `.` as part of variable names, usually to indicate the
    type of the variable. eg. `col.shadow` means shadow colour. However,
    `shadow` is not a nested attribute of `col`.

--- flake.nix
+++ flake.nix
@@ -1994,10 +1994,9 @@ bar {
               gaps_in = 5;
               gaps_out = 20;
               border_size = 2;
-              col = {
-                active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-                inactive_border = "rgba(595959aa)";
-                layout = "dwindle";
+              "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+              "col.inactive_border" = "rgba(595959aa)";
+              "col.layout" = "dwindle";
               };
             };
             decoration = {
@@ -2012,7 +2011,7 @@ bar {
               drop_shadow = true;
               shadow_range = 4;
               shadow_render_power = 3;
-              col.shadow = "rgba(1a1a1aee)";
+              "col.shadow" = "rgba(1a1a1aee)";
             };
             animations = {
               enabled = true;
