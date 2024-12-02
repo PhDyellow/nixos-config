5b0ecb11bf9e8d1fc6dd29cd12a1f33483bd94aa
Phil Dyer <phildyer@protonmail.com>
2024-12-03 02:29:01 +1000

    Add awesomecv latex package and dont use custom moderncv tex package

--- flake.nix
+++ flake.nix
@@ -1850,6 +1850,7 @@ bar {
 
         texlive-system = {config, pkgs, ...}:
           let
+            # moderncv is already in TeXLive
             moderncv = pkgs.stdenvNoCC.mkDerivation {
                 pname = "latex-moderncv";
                 version = "2.0.0";
@@ -1885,7 +1886,52 @@ bar {
                 runHook postInstall
                 '';
                };
-                 # awesomecv =
+            awesomecv = pkgs.stdenvNoCC.mkDerivation {
+                pname = "latex-awesome-cv";
+                version = "1.6.1";
+
+                outputs = [  "tex" "out"  ];
+
+                passthru.tlDeps = with pkgs.texlive; [
+                  latex
+                  luatex
+                  roboto
+                  fontawesome5
+                  xstring
+                  xifthen
+                  xcolor
+                  enumitem
+                  ragged2e
+                  geometry
+                  fancyhdr
+                  etoolbox
+                  setspace
+                  unicode-math
+                  sourcesanspro
+                  tcolorbox
+                  parskip
+                  hyperref
+                  bookmark
+                ];
+
+                src = inputs.awesomecv;
+
+                installPhase = ''
+                runHook preInstall
+
+                path="$tex/tex/latex/awesome-cv/"
+                mkdir -p "$path"
+                cp "$src/awesome-cv.cls" "$path"
+
+                # $out must be defined, but does not
+                # need to be used.
+                # By putting `out` second in `outputs`,
+                # `out` will not be the default
+                mkdir -p $out
+
+                runHook postInstall
+                '';
+               };
             altacv = pkgs.stdenvNoCC.mkDerivation {
                 pname = "latex-altacv";
                 version = "1.1.3";
@@ -1927,7 +1973,7 @@ bar {
 
             texlive-extended = pkgs.texliveFull.withPackages (ps:  [
               altacv
-              moderncv
+              awesomecv
             ]);
           in {
             environment.systemPackages = with pkgs; [
