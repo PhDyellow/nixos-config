751fe828caf3836d68ddfdce257f2803c15af7e7
Phil Dyer <phildyer@protonmail.com>
2024-12-02 23:19:14 +1000

    Add comment for citar fix

--- flake.nix
+++ flake.nix
@@ -2423,6 +2423,7 @@ bar {
             };
             init = {
               enable = true;
+              ## TODO as of 2024-12-02 and emacs 31, package-quickstart breaks citar
               packageQuickstart = false;
               recommendedGcSettings = true;
               startupTimer = true;
