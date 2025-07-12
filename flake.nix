a9140753da32d4981af9916f4f34d5bd028f1055
Phil Dyer <phildyer@protonmail.com>
2025-07-12 17:08:52 +1000

    emacs-packages consult: set preview key to `M-]`
    Prevent hanging when I tap past a network file or large file such as
    image or PDF.

--- flake.nix
+++ flake.nix
@@ -3223,18 +3223,30 @@ bar {
                           ;; Optionally configure preview. The default value
                           ;; is 'any, such that any key triggers the preview.
                           ;; (setq consult-preview-key 'any)
-                          ;; (setq consult-preview-key (kbd "M-."))
-                          ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+                          ;; (setq consult-preview-key (kbd "M-]"))
+                          (setq consult-preview-key (list "M-]"))
                           ;; For some commands and buffer sources it is useful to configure the
                           ;; :preview-key on a per-command basis using the `consult-customize' macro.
                           (consult-customize
-                          consult-theme :preview-key '(:debounce 0.2 any)
-                          consult-ripgrep consult-git-grep consult-grep
-                          consult-bookmark consult-recent-file consult-xref
-                          consult--source-bookmark consult--source-file-register
-                          consult--source-recent-file consult--source-project-recent-file
-                          ;; :preview-key (kbd "M-.")
-                          :preview-key '(:debounce 0.4 any))
+                          consult-theme
+                          consult-buffer
+                          consult-ripgrep
+                          consult-git-grep
+                          consult-grep
+                          consult-bookmark
+                          consult-fd
+                          consult-locate
+                          consult-recent-file
+                          consult-xref
+                          consult--source-bookmark
+                          consult--source-file-register
+                          consult--source-recent-file
+                          consult--source-project-recent-file
+                          consult-notes
+                          consult-dir
+                          consult-recoll
+                          consult-org
+                          )
 
                           ;; Optionally configure the narrowing key.
                           ;; Both < and C-+ work reasonably well.
